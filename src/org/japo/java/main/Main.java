abb0db0a02940a9b1201150ee3bf4445959a6a52
eli <eli@192.168.1.134>
2019-10-26 19:29:04 +0200

    :game_die: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+
+    public static final Random RND = new Random();
+
+    public static void main(String[] args) {
+
+        //Constantes
+        final int DADO_MAX = 6;
+        final int DADO_MIN = 1;
+
+        //variable
+        int tirada;
+
+        //tirada
+        tirada = RND.nextInt(DADO_MAX - DADO_MIN + 1) + DADO_MIN;
+
+        System.out.printf("Tirada dado.......: %d%n", tirada);
+    }//main
+
+}//class
